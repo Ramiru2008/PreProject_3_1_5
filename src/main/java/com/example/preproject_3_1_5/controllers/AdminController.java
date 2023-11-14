262a452fabecc2f52013cc875d21f544b8e88bce
Roman Kudasov <rkudasov@gmail.com>
2023-11-14 11:40:20 +0300

    Alpha Version

--- src/main/java/com/example/preproject_3_1_5/controllers/AdminController.java
+++ src/main/java/com/example/preproject_3_1_5/controllers/AdminController.java
@@ -1,16 +1,14 @@
 package com.example.preproject_3_1_5.controllers;
 
-import com.example.preproject_3_1_5.exception_handling.NoSuchUserException;
 import com.example.preproject_3_1_5.dto.UserDto;
 import com.example.preproject_3_1_5.entities.Role;
 import com.example.preproject_3_1_5.entities.User;
 import com.example.preproject_3_1_5.services.RoleService;
 import com.example.preproject_3_1_5.services.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
-
-
 import java.util.*;
 
 
@@ -19,48 +17,48 @@ import java.util.*;
 public class AdminController {
     private final UserService userService;
     private final RoleService roleService;
-    private final PasswordEncoder passwordEncoder;
 
-    public AdminController(UserService userService, RoleService roleService, PasswordEncoder passwordEncoder) {
+    public AdminController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
-        this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/users")
-    public List<User> show() {
-        List<User> users = userService.getAllUsers();
-        return users;
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
     @PostMapping(value = "/users")
-    public User create(@RequestBody UserDto userDto) {
+    public ResponseEntity<User> create(@RequestBody UserDto userDto) {
         List<Role> roles = roleService.getRoleByIds(userDto.getRoles());
-            User user = new User(
-                    userDto.getUsername(),
-                    userDto.getFirstName(),
-                    userDto.getLastName(),
-                    userDto.getAge(),
-                    userDto.getPassword(),
-                    new HashSet<>(roles)
-            );
-            userService.add(user);
-        return user;
+        User user = new User(
+                userDto.getUsername(),
+                userDto.getFirstName(),
+                userDto.getLastName(),
+                userDto.getAge(),
+                userDto.getPassword(),
+                new HashSet<>(roles)
+        );
+        userService.add(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @GetMapping("/users/{id}")
-    public User getUserById(@PathVariable("id") Long id) {
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
         User user = userService.getUserById(id);
         if (user == null) {
-            throw new NoSuchUserException("There is no User with ID = " + id + " in Database");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return user;
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 
     @PutMapping("/users")
-    public User editUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<String> editUser(@RequestBody UserDto userDto) {
         User user = userService.getUserById(userDto.getId());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
         List<Role> roles = roleService.getRoleByIds(userDto.getRoles());
         Set<Role> setRoles = new HashSet<>(roles);
         user.setRoles(setRoles);
@@ -69,16 +67,15 @@ public class AdminController {
         user.setUsername(userDto.getUsername());
         user.setAge(userDto.getAge());
         if (!userDto.getPassword().isEmpty()) {
-       userService.edit(user);
+            userService.edit(user);
         }
-       return user;
+        return ResponseEntity.ok("User updated successfully");
     }
 
     @DeleteMapping("/users/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
         userService.removeUserById(id);
-
-        return "User with ID = " + id + " was deleted";
+        return new ResponseEntity<>("User with ID = " + id + " was deleted", HttpStatus.OK);
     }
 
 }
